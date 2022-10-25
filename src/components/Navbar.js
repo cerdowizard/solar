57e117d8bd1bf4efa8fd98ad0f62ad75a82a8688
cerdowizard <holuwayinkzzy@gmail.com>
2022-10-25 15:12:07 +0100

    navbar fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,44 +1,26 @@
-import React from 'react'
+import img from '../assets/Helios Limited No BG Black-01 1.png'
+import img2 from '../assets/ri_customer-service-2-line.png'
 import '../styles/home.scss'
 const Navbar = () => {
   return (
-      <div id='Navbar'>
-          <nav class="navbar navbar-expand-lg">
-              <div class="container-fluid">
-                  <a class="navbar-brand" href="#">Navbar</a>
-                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                      <span class="navbar-toggler-icon"></span>
-                  </button>
-                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                          <li class="nav-item">
-                              <a class="nav-link active" aria-current="page" href="#">Home</a>
-                          </li>
-                          <li class="nav-item">
-                              <a class="nav-link" href="#">Link</a>
-                          </li>
-                          <li class="nav-item dropdown">
-                              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                  Dropdown
-                              </a>
-                              <ul class="dropdown-menu">
-                                  <li><a class="dropdown-item" href="#">Action</a></li>
-                                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                                  <li><hr class="dropdown-divider"/></li>
-                                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                              </ul>
-                          </li>
-                          <li class="nav-item">
-                              <a class="nav-link disabled">Disabled</a>
-                          </li>
-                      </ul>
-                      <form class="d-flex" role="search">
-                          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                              <button class="btn btn-outline-success" type="submit">Search</button>
-                      </form>
-                  </div>
-              </div>
-          </nav>
+    <div id='Navbar'>
+      <nav>
+        <div className="left">
+          <img src={img} alt="" />
+          <span> Helios Energeia</span>
+         
+        </div>
+        <div className="rigth">
+          <ul>
+            <li>Home</li>
+            <li>Services</li>
+            <li>Projects</li>
+            <li>About</li>
+            <li className='Contact'><img src={img2} alt="" /> Contact US</li>
+          </ul>
+
+        </div>
+      </nav>
     </div>
   )
 }
